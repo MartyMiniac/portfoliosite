d6118aaa011f47423c3cc0edf1dbd6b964bdcbac
Rohan Verma <rohanvermaara@gmail.com>
2020-11-19 11:53:31 +0530

    added contact page need to implement db side in app.py

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/contact',methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+@app.route('/contactpost',methods=['POST'])
+def contactpost():
+    print(request.form['name'])
+    print(request.form['email'])
+    print(request.form['msg'])
+    return 'received'
+
 if __name__ == '__main__':
     app.run(debug=True)
